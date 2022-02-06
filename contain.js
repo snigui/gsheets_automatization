7b5bedd34c23d72884e82fc03e459eaddba147f0
Snigdha Motadaka <40993241+snigui@users.noreply.github.com>
2022-02-06 17:12:29 -0500

    Create contain.js

---
+++ contain.js
@@ -0,0 +1,25 @@
+function contains() {
+  var ss = SpreadsheetApp.getActiveSpreadsheet();
+  var sheet = ss.getActiveSheet();
+  var data = sheet.getRange('A1:A').getValues().filter(String);
+  var liveData = sheet.getRange('B1:B').getValues().filter(String);
+  //why just why is the getValue a 2D array that cant be flattened why
+  //flattening it myself because someone had to do it smh
+  data = (JSON.stringify(data)).replace(/\[*\]*"*/g,"")
+  liveData = (JSON.stringify(liveData)).replace(/\[*\]*"*/g,"")
+  //i had to learn regex again for this
+  //i swear google should hire me at this point
+  data = data.split(",")
+  liveData = liveData.split(",")
+  var missingData = []
+  for (var i = 0; i < data.length; i++){
+    if (!(liveData.includes(data[i]))){
+      missingData.push([data[i]])
+    }
+  }
+  console.log(missingData)
+  console.log(liveData)
+  var cell = sheet.getRange("C1:G"+(missingData.length+1));
+  cell.clear()
+  cell.setValues(missingData);
+}
